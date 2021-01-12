45e472a649e78672f3e3b86943a00a02f1fcbb27
Александр Хрусталев <returnXAA@outlook.com>
2021-01-12 20:16:10 +0500

    Creat Registry Authorisation

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['status','auth']], function(){
+    $groupData=[
+      'namespace'=>'Blog\Admin',
+      'prefix'=>'admin',
+    ];
+
+    Route::group($groupData, function (){
+       Route::resource('index','MainController')->names('blog.admin.index') ;
+    });
+});
