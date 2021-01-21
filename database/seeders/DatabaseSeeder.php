63ac397858e7ee5eada1af1dd4bbaf95bde1cf67
Александр Хрусталев <returnXAA@outlook.com>
2021-01-21 19:00:15 +0500

    Create Login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,7 @@
 
 namespace Database\Seeders;
 
-
 use Illuminate\Database\Seeder;
-
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,19 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(RolesTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
-        $this->call(UserRolesTableSeeder::class);
-        $this->call(AttributeGroupsSeeder::class);
-        $this->call(AttributeProductsSeeder::class);
-        $this->call(AttributeValuesSeeder::class);
-        $this->call(BrandsSeeder::class);
-        $this->call(CategoriesSeeder::class);
-        $this->call(CurrenciesSeeder::class);
-        $this->call(GalleriesSeeder::class);
-        $this->call(ProductsSeeder::class);
-        $this->call(RelatedProductsSeeder::class);
-        $this->call(OrdersSeeder::class);
-        $this->call(AdminOrderProductsSeeder::class);
+        $this->call(UsersSeed::class);
+        // \App\Models\User::factory(10)->create();
     }
 }
