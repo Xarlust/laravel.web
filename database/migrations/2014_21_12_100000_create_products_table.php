9613d406be227e315954f0d5985f24380b60f8fa
Александр Хрусталев <returnXAA@outlook.com>
2021-01-23 14:25:35 +0500

    (edit front) ProductController,crete IndexController

--- database/migrations/2014_21_12_100000_create_products_table.php
+++ database/migrations/2014_21_12_100000_create_products_table.php
@@ -16,8 +16,8 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('price');
-            $table->string('descriptione');
+            $table->unsignedInteger('price');
+            $table->string('description');
             $table->timestamps();
         });
     }
